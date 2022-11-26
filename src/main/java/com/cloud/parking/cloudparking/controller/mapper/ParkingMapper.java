810068a8e13d233db31460fdbebfa5f9c5dced91
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2022-11-26 20:18:14 -0300

    Mapper parking

---
+++ src/main/java/com/cloud/parking/cloudparking/controller/mapper/ParkingMapper.java
@@ -0,0 +1,32 @@
+package com.cloud.parking.cloudparking.controller.mapper;
+
+import com.cloud.parking.cloudparking.controller.dto.ParkingCreateDTO;
+import com.cloud.parking.cloudparking.controller.dto.ParkingDTO;
+import com.cloud.parking.cloudparking.model.Parking;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ParkingMapper {
+
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public ParkingDTO toParkingDTO(Parking parking) {
+        return MODEL_MAPPER.map(parking, ParkingDTO.class);
+    }
+
+    public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList) {
+        return parkingList.stream().map(this::toParkingDTO).collect(Collectors.toList());
+    }
+
+    public Parking toParking(ParkingDTO dto) {
+        return MODEL_MAPPER.map(dto, Parking.class);
+    }
+
+    public Parking toParkingCreate(ParkingCreateDTO dto) {
+        return MODEL_MAPPER.map(dto, Parking.class);
+    }
+}
