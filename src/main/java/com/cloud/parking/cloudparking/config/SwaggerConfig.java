703789f4b01085b5ff87d316d03d4a12c52fdd44
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2022-11-26 20:10:11 -0300

    Security configuration

--- src/main/java/com/cloud/parking/cloudparking/config/SwaggerConfig.java
+++ src/main/java/com/cloud/parking/cloudparking/config/SwaggerConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Component
 @EnableSwagger2
@@ -33,5 +35,23 @@ public class SwaggerConfig {
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
                 .build();
+    }
+
+    private SecurityScheme basicAuthScheme() {
+        return new BasicAuth("basicAuth");
+    }
+
+    private SecurityReference basicAuthReference() {
+        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+    }
+
+    private List<SecurityScheme> basicScheme() {
+        List<SecurityScheme> schemeList = new ArrayList<>();
+        schemeList.add(new BasicAuth("basicAuth"));
+        return schemeList;
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("apiKey", "Authorization", "header");
     }
 }
---
+++ src/main/java/com/cloud/parking/cloudparking/controller/dto/ParkingCreateDTO.java
@@ -0,0 +1,4 @@
+package com.cloud.parking.cloudparking.controller.dto;
+
+public class ParkingCreateDTO {
+}
